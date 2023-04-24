09bd3a901b5857a94c4476aeb98a9ac8f8b799db
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2023-04-23 19:58:15 -0500

    input image

---
+++ src/indep.js
@@ -0,0 +1,14 @@
+console.log("aaaa")
+const input = document.getElementById('imagen')
+const test = document.getElementById("test")
+input.addEventListener("change",changeI )
+
+function changeI(){
+    console.log("change")
+    let file = input.files[0]
+    console.log(file)
+    let image = document.createElement('img');
+    console.log(URL.createObjectURL(file))
+    image.src = URL.createObjectURL(file);
+    test.appendChild(image)
+}
